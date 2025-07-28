779738d06d75c03795454bb5ed523ab65ad5d1ff
RealtalkAI552 <dheeraj80548@gmail.com>
2025-07-28 20:44:19 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome to RealTalk Voice Transcriber</h1>
+      <p>This is the landing page. Upload a voice file to test transcription.</p>
+    </div>
+  )
+}
